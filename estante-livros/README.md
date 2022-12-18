26dba3950f8245730efd83d3231a8252313b3e67
guilhermehub12 <guilhermee1312@hotmail.com>
2022-12-18 17:09:26 -0300

    atividade estante de livros finalizada

---
+++ estante-livros/README.md
@@ -0,0 +1,28 @@
+# Estante de Livros Virtual - HappyCode
+
+## Preview
+
+[![Creative Preview](https://assets.startbootstrap.com/img/screenshots/themes/creative.png)](https://startbootstrap.github.io/startbootstrap-creative/)
+
+**[View Live Preview](https://startbootstrap.github.io/startbootstrap-creative/)**
+
+## Status
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/StartBootstrap/startbootstrap-creative/master/LICENSE)
+[![npm version](https://img.shields.io/npm/v/startbootstrap-creative.svg)](https://www.npmjs.com/package/startbootstrap-creative)
+[![dependencies Status](https://david-dm.org/StartBootstrap/startbootstrap-creative/status.svg)](https://david-dm.org/StartBootstrap/startbootstrap-creative)
+[![devDependencies Status](https://david-dm.org/StartBootstrap/startbootstrap-creative/dev-status.svg)](https://david-dm.org/StartBootstrap/startbootstrap-creative?type=dev)
+
+## Download and Installation
+
+To begin using this template, choose one of the following options to get started:
+
+- [Download the latest release on Start Bootstrap](https://startbootstrap.com/theme/creative/)
+- Install using npm: `npm i startbootstrap-creative`
+- Clone the repo: `git clone https://github.com/StartBootstrap/startbootstrap-creative.git`
+- [Fork, Clone, or Download on GitHub](https://github.com/StartBootstrap/startbootstrap-creative)
+
+
+## Copyright and License
+
+Copyright 2013-2022 Start Bootstrap LLC. Code released under the [MIT](https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE) license.
---
+++ estante-livros/dist/js/scripts.js
@@ -0,0 +1,54 @@
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Encolher a barra de navegação 
+    navbarShrink();
+
+    // Encolher a barra de navegação quando a página é rolada
+    document.addEventListener('scroll', navbarShrink);
+
+    // Ativa o bootstrap scrollspy no elemento de navegação principal
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Recolher a barra de navegação responsiva quando o alternador estiver visível
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Ativa o plug-in SimpleLightbox para itens do portfólio
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
